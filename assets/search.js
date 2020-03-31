30a388e108275b50c32a4105b004fe1b4ac224e3
Tejas Vedantham <tejas6080@gmail.com>
2020-03-30 23:27:08 -0400

    Added bhajan card template functionality

--- assets/search.js
+++ assets/search.js
@@ -6,14 +6,25 @@ $('#search-online').click(function() {
    shell.openExternal('https://www.google.com/search?q=' + $('.prompt').val());
 });
 
-newHtml = $("<div class='ui raised card'>")
-   .append($("<div class='content'>")
-      .append("<div class='header'>Sample</div>")
-      .append("<div class='meta'>Friend</div>")
-      .append("<div class='description'>Sample Sample Sample</div>"));
+// newHtml = $("<div class='ui raised card'>")
+//    .append($("<div class='content'>")
+//       .append("<div class='header'>Sample</div>")
+//       .append("<div class='meta'>Friend</div>")
+//       .append("<div class='description'>Sample Sample Sample</div>"));
       
 $("#search-bar").on("input", function() {
-   // $(".ui.cards").append(newHtml);
+
+   $.get("../bhajan-card-template.html", function(data) {
+      $('.ui.cards').append(data);
+   });
+
    var bhajan = $('.prompt').val();
+   
+   //Only perform search if there are >3 chars in the query
+   if (bhajan.length < 3) {
+      return;
+   }
+
+   //Send call to retrieve data from db
    ipcRenderer.send('bhajan-to-search', bhajan);
 });
