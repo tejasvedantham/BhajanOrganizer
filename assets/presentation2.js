d87ac5a9c2d9d04e2de4b04980bd6896f4edd94c
Tejas Vedantham <tejas6080@gmail.com>
2020-04-04 01:00:41 -0400

    Added form submit and JSON rendering functionality

--- assets/presentation2.js
+++ assets/presentation2.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     const { ipcRenderer } = require('electron');
+    const { fs } = require('fs');
     var nodeConsole = require('console');
     var myConsole = new nodeConsole.Console(process.stdout, process.stderr);
     
@@ -17,6 +18,22 @@ $(document).ready(function() {
     $('#add-bhajan-button').click(function() {
         $('.ui.modal').modal('show');
     })
+
+    $('.ui.form').submit(function(event) {
+        myConsole.log("-----Submitted-----");
+        var bhajanArray = $('.ui.form').serializeArray();
+        bhajanArray = objectifyForm(bhajanArray);
+        myConsole.log(bhajanArray);
+    });
+
+    function objectifyForm(formArray) {
+
+        var returnArray = {};
+        for (var i = 0; i < formArray.length; i++){
+          returnArray[formArray[i]['name']] = formArray[i]['value'];
+        }
+        return returnArray;
+    }
 });
 
 
