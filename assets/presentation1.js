7de01a252fa0b4b840fa4dc9aa1f6b6c99412b07
Tejas Vedantham <tejas6080@gmail.com>
2020-04-24 16:00:07 -0400

    Added dialog open files

--- assets/presentation1.js
+++ assets/presentation1.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     const { ipcRenderer } = require('electron');
+    const { dialog } = require('electron').remote;
     var nodeConsole = require('console');
     var myConsole = new nodeConsole.Console(process.stdout, process.stderr);
     
@@ -7,5 +8,22 @@ $(document).ready(function() {
         $('#page-content').load("../html_pages/presentation2.html", function() {
             $.getScript("../assets/presentation2.js");
         });
+    });
+
+    $('#create-button').click(function() {
+        $('#page-content').load("../html_pages/presentation2.html", function() {
+            $.getScript("../assets/presentation2.js");
+        });
+    });
+
+    $('#open-button').click(function() {
+        dialog.showOpenDialog({
+            properties: ['openFile']
+        }).then(result => {
+            myConsole.log(result.canceled);
+            myConsole.log(result.filePaths);
+        }).catch(err => {
+            myConsole.log(err);
+        })
     });
 });
